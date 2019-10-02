2c84ae31fe46489da863981d549aaac07d7a4db9
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-10-02 15:02:45 -0600

    test: update snapshot

--- src/layouts/Base.test.tsx
+++ src/layouts/Base.test.tsx
@@ -5,39 +5,10 @@ import Layout from "./Base";
 
 beforeEach((): void => {
   (useStaticQuery as jest.Mock).mockImplementation((): object => ({
-    file: {
-      childImageSharp: {
-        fixed: {
-          base64: "",
-          width: 100,
-          height: 100,
-          src: "",
-          srcSet: ""
-        }
-      }
-    },
-    backdrop: {
-      childImageSharp: {
-        fluid: {
-          base64: "",
-          aspectRatio: 1.333,
-          src: "",
-          srcSet: "",
-          srcWebp: "",
-          srcSetWebp: "",
-          sizes: ""
-        }
-      }
-    },
     site: {
       siteMetadata: {
         title: "",
-        description: "",
-        siteUrl: "",
-        twitterUsername: "",
-        instagramUsername: "",
-        linkedinUsername: "",
-        githubUsername: ""
+        siteUrl: ""
       }
     }
   }));
