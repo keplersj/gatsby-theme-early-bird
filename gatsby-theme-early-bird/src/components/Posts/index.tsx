a53c188f74a47c2264f5990345c9e193f92c5cdd
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-12-24 22:35:55 -0700

    refactor(PostsPage): use fragment for posts

--- gatsby-theme-early-bird/src/components/Posts/index.tsx
+++ gatsby-theme-early-bird/src/components/Posts/index.tsx
@@ -106,3 +106,44 @@ export const Posts = ({ data, location }: Props): React.ReactElement<Props> => {
     </BaseLayout>
   );
 };
+
+export const fragment = graphql`
+  fragment EarlyBirdPostsPage on BlogPost {
+    id
+    excerpt
+    slug
+    title
+    isoDate: date
+    date(formatString: "MMMM DD, YYYY")
+    # tags
+    featuredImage {
+      childImageSharp {
+        # Generate Picture up to 8K 4:3 ratio, crop and cover as appropriate
+        fluid(
+          maxWidth: 7680
+          maxHeight: 5760
+          cropFocus: CENTER
+          fit: COVER
+          srcSetBreakpoints: [
+            256
+            512
+            768
+            1024
+            # 720p
+            1280
+            # 1080p
+            1920
+            # 4k
+            3840
+            # 5k
+            5120
+            # 8k
+            7680
+          ]
+        ) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`;
